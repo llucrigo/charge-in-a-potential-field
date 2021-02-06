9a2c50eeec95b2f2e779ec94312d00ba56b290c8
Lluc Rigo <mariadellucrigo@gmail.com>
2021-02-06 21:47:37 +0100

    Several mobile charges

--- projecte.py
+++ projecte.py
@@ -5,8 +5,6 @@ Created on Thu Feb  4 20:57:48 2021
 @author: lluc
 """
 
-"""Plots field lines for dipole."""
-
 #from matplotlib import pyplot
 import matplotlib.pyplot as plt
 import numpy
@@ -35,6 +33,8 @@ class MobileCharge_class:
 	#Charge moving in the electric field
 
 	h=0.1 #Time step
+	eps=0.1
+	nmax=50
 
 	def __init__(self, electricfield, q, m, r, v):
 		self.electricfield= electricfield
@@ -46,52 +46,46 @@ class MobileCharge_class:
 	def move(self):
 		E=self.electricfield.vector([self.r[0],self.r[1]])
 		ac=self.q*E/self.m
-		self.r[0]=self.r[0]+self.v[0]*self.h+(ac[0]/2)*self.h**2
-		self.r[1]=self.r[1]+self.v[1]*self.h+(ac[1]/2)*self.h**2
-		self.v[0]=self.v[0]+ac[0]*self.h
-		self.v[1]=self.v[1]+ac[1]*self.h
+		n=1
+		h=self.h
+		nr=[0,0]
+		while n<self.nmax:
+			nr[0]=self.r[0]+self.v[0]*h+(ac[0]/2)*h**2
+			nr[1]=self.r[1]+self.v[1]*h+(ac[1]/2)*h**2
+			d=((nr[0]-self.r[0])**2+(nr[1]-self.r[1])**2)**.5
+			if (d<self.eps):
+				self.r=nr
+				self.v[0]=self.v[0]+ac[0]*h
+				self.v[1]=self.v[1]+ac[1]*h
+				return n
+			else:
+				h=h/2
+			n=n+1
+		self.r=nr
+		self.v[0]=self.v[0]+ac[0]*h
+		self.v[1]=self.v[1]+ac[1]*h
+		return n
 
 	def get_r(self):
 		return self.r
 
+	def plot(self,color):
+		plt.scatter(self.r[0],self.r[1], 1, color)
+
+	def checkpoint(self,xmin, xmax, ymin, ymax):
+		if self.r[0]>xmax or self.r[0]<xmin or self.r[1]>ymax or self.r[1]<ymin:
+			return True
+
 
 config=getFileData("config.txt")
 
-# config=[]
-
-# with open('config.txt','r') as config_file:
-# 	data=config_file.readline()
-	
-# 	for data in config_file:
-# 		if data.startswith('#'):
-# 			data.rstrip()
-# 		else:
-# 			config.append(float(data.rstrip()))
-
-#print(config)
-
-
-XMIN, XMAX = config[0],config[1]
-YMIN, YMAX = config[2],config[3]
-ZOOM = config[4]
-XOFFSET = config[5]
+XMIN, XMAX, YMIN, YMAX, ZOOM, XOFFSET=config
 
 
 electrostatics.init(XMIN, XMAX, YMIN, YMAX, ZOOM, XOFFSET)
 
+charges1=getFileData("charges.txt")
 
-charges1=[]
-with open('charges.txt','r') as charges_file:
-	
-	info=charges_file.readline()
-	
-	for info in charges_file:
-		if info.startswith('#'):
-			info.rstrip()
-		else:
-			charges1.append(float(info.rstrip()))
-
-#print(len(charges1))
 a=int(len(charges1)/3)
 #print(a)
 
@@ -118,29 +112,30 @@ potential = Potential(charges)
 
 
 #MobileCharge
-MobileCharge=[]
-with open('mobilecharge.txt','r') as mobilecharge_file:
-	
-	carac=mobilecharge_file.readline()
-	
-	for carac in mobilecharge_file:
-		if carac.startswith('#'):
-			carac.rstrip()
-		else:
-			MobileCharge.append(float(carac.rstrip()))
+MobileCharge=getFileData("mobilecharge.txt")
 
 #print(MobileCharge)
-q_mc=MobileCharge[0]
-m_mc=MobileCharge[1]
-x_mc=MobileCharge[2]
-y_mc=MobileCharge[3]
-vx_mc=MobileCharge[4]
-vy_mc=MobileCharge[5]
-#h=1e-1
 
-mobilecharge = MobileCharge_class(field,q_mc,m_mc,[x_mc,y_mc],[vx_mc,vy_mc])
+q_mc=[]
+m_mc=[]
+x_mc=[]
+y_mc=[]
+vx_mc=[]
+vy_mc=[]
+mobilecharge=[]
+
+for i in range(0, int(len(MobileCharge)/6)): 
+	q_mc.append(MobileCharge[i*6])
+	m_mc.append(MobileCharge[i*6+1])
+	x_mc.append(MobileCharge[i*6+2])
+	y_mc.append(MobileCharge[i*6+3])
+	vx_mc.append(MobileCharge[i*6+4])
+	vy_mc.append(MobileCharge[i*6+5])
+	mobilecharge.append(MobileCharge_class(field,q_mc[i],m_mc[i],[x_mc[i],y_mc[i]],[vx_mc[i],vy_mc[i]]))
 
 ## Plotting ##
+
+colors=["#00ff00","#ff0000","#0000ff","#f00f00","#0f00f0","#00f00f"]
 
 # Electric field lines and potential contours
 
@@ -148,23 +143,22 @@ fig = plt.figure(figsize=(6, 4.5))
 potential.plot()
 field.plot()
 
-n=0
-while n<500:
-	x_mc,y_mc=mobilecharge.get_r()
-	plt.scatter(x_mc,y_mc, 2, "#00ff00")
-	mobilecharge.move()
-	#plt.scatter(x_mc,y_mc, 2, "#00ff00")
-	# E=field.vector([x_mc,y_mc])
-	# ac=q_mc*E/m_mc
-	# x_mc=x_mc+vx_mc*h+(ac[0]/2)*h**2
-	# vx_mc=vx_mc+ac[0]*h
-	# y_mc=y_mc+vy_mc*h+(ac[1]/2)*h**2
-	# vy_mc=vy_mc+ac[1]*h
-	n=n+1
+for i in range(0, len(mobilecharge)):
+	n=0
+	while n<500:
+		if mobilecharge[i].checkpoint(XMIN, XMAX, YMIN, YMAX):
+			break
+		else:
+			x_mc,y_mc=mobilecharge[i].get_r()
+			mobilecharge[i].plot(colors[i])
+			m=mobilecharge[i].move()
+			r=mobilecharge[i].get_r()
+			#print(n,m,r)
+			n=n+1
+	print(i, n)
 
 for charge in charges:
 		charge.plot()
 finalize_plot()
-
 
 plt.show()
